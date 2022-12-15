e325f0b9c1af71492a8d3b5ce68b8c05a29ade35
A Stone <astone@heckgrammar.co.uk>
2022-12-15 13:33:21 +0000

    Create Random Numbers in VB

--- GenerateRandomNumbers/Module1.vb
+++ GenerateRandomNumbers/Module1.vb
@@ -1,7 +1,11 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Dim rand As New Random
+        For i = 1 To 100
+            Dim n As Integer = rand.Next(1, 100)
+            Console.WriteLine(n)
+        Next
     End Sub
 
 End Module
