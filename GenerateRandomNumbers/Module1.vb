c5da8e7acf06c832c1d26404de47e559b7eba467
A Stone <astone@heckgrammar.co.uk>
2022-12-16 09:23:59 +0000

    Generate Randomdess

--- GenerateRandomNumbers/Module1.vb
+++ GenerateRandomNumbers/Module1.vb
@@ -1,11 +1,14 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim rand As New Random
+        Dim rand As New Random ' plants the seed to generate random numbers
+
         For i = 1 To 100
-            Dim n As Integer = rand.Next(1, 100)
-            Console.WriteLine(n)
+            Dim n As Integer
+            n = rand.Next(1, 100) ' generate a number from 1 to 99
+            Console.Write(n & " ")
         Next
+
     End Sub
 
 End Module
