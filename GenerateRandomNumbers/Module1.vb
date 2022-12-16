b661546de9a22efc9837cb85a72b8092f5f30a45
flymo5678 <flynn@littlebricks.org>
2022-12-16 09:36:28 +0000

    EVEN MORE RANDOM!!!!!!!!!!!!

--- GenerateRandomNumbers/Module1.vb
+++ GenerateRandomNumbers/Module1.vb
@@ -3,7 +3,7 @@
     Sub Main()
         Dim rand As New Random ' plants the seed to generate random numbers
 
-        For i = 1 To 10000
+        For i = 1 To 1000000 'generates so much random
             Dim n As Integer
             n = rand.Next(1, 100) ' generate a number from 1 to 99
             Console.Write(n & " ")
